654f9d3b047abeff9ee2b02696385ab4c95591da
Yoga Bayu <bayu9yoga2@gmail.com>
2025-08-23 04:16:20 +0700

    first clean commit

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load .env file
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Bot is online as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
